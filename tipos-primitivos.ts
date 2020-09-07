265f8fde29312d29eb4f2c7f75a140093ea73c8a
Enrique Sotelo Ponce <sotelo.enrique@gmail.com>
2020-09-07 18:07:47 -0500

    Curso TypeScript de Makigas video 06

---
+++ tipos-primitivos.ts
@@ -0,0 +1,14 @@
+/* boolean */
+let logico: boolean = false
+let verdadero: boolean = true
+
+/* number */
+let entero: number = 44
+let decimal: number = 26.05
+let hexadecimal: number = 0xcbcbaa
+let octal: number = 0o7761
+let binario: number = 0b110011001
+let enteroLargo: number = 7_812_341_121
+
+/* string */
+let cadenas: string = "ksndskdnlsmñdfmlksandfkdsn"
