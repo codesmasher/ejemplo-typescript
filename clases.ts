61df830dee55ea68905246792583ac6539c3cb42
Enrique Sotelo Ponce <sotelo.enrique@gmail.com>
2020-09-07 21:47:34 -0500

    Curso TypeScript de Makigas video 16

--- clases.ts
+++ clases.ts
@@ -1,16 +1,30 @@
 // Declaración de clase
 // Agregar visibilidad a las propiedades de la clase
 class Rectangulo {
-    readonly alto: number
-    readonly ancho: number
+    alto: number
+    ancho: number
+    private _nombre: string
+
     constructor(ancho: number, alto: number) {
         console.log("constructor()")
         this.ancho = ancho
         this.alto = alto
     }
-    area() {
+    // Getters / Setters
+    get nombre() {
+        console.log("Obtengo el nombre")
+        return this._nombre
+    }
+
+    set nombre(value: string) {
+        console.log("Seteo el nombre")
+        this._nombre = value
+    }
+
+    get area() {
         return this.ancho * this.alto
     }
+
     perimetro() {
         return (this.ancho + this.alto) * 2
     }
@@ -19,3 +33,6 @@ class Rectangulo {
 let iRectangulo = new Rectangulo(1, 2)
 // iRectangulo.alto no esta permitido por ser una propiedad privada
 // iRectangulo.alto = 12 no esta permitido por ser una propiedad solo lectura
+console.log(`El área del rectangulo es: ${iRectangulo.area}`)
+iRectangulo.nombre = "A"
+console.log(`El rectángulo ${iRectangulo.nombre}`)
