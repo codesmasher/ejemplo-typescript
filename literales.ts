3fb1849df3f168cba1e3f75e149085b6a7d248d8
Enrique Sotelo Ponce <sotelo.enrique@gmail.com>
2020-09-08 12:30:42 -0500

    Curso TypeScript de Makigas video 20

---
+++ literales.ts
@@ -0,0 +1,8 @@
+let verdadero: true = true // Esto es un tipo literal
+
+const falso = false // Esto es una constante
+
+verdadero = falso // Esto no es posible, porque al declarar verdadero como true
+                  // esta variable no puede ser re asignada. Por lo que un
+                  // literal funciona igual que una constante.
+
