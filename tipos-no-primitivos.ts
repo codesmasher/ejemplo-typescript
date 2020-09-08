fc34df8a53ef8f53cb7082de8a8025a1675423a5
Enrique Sotelo Ponce <sotelo.enrique@gmail.com>
2020-09-07 19:35:02 -0500

    Curso TypeScript de Makigas video 09

--- tipos-no-primitivos.ts
+++ tipos-no-primitivos.ts
@@ -3,3 +3,28 @@ let dias: string[] = ["lunes", "martes", "miercoles", "jueves", "viernes"]
 
 // tuplas
 let db: [string, number, string, boolean] = ["Enrique", 40, "México", true]
+
+// objetos
+let persona: {
+    nombre: string,
+    edad: number,
+    pais: string,
+    fecha_nac: Date,
+    mascota: boolean,
+    laboral: {
+        empresa: string,
+        sector: string
+    }
+} = {
+    nombre: "Pedro Perez",
+    edad: 30,
+    pais: "Colombia",
+    fecha_nac: new Date(1990, 5, 14),
+    mascota: true,
+    laboral: {
+        empresa: "Empresita",
+        sector: "Informática"
+    }
+}
+// declaración poco conveniente y es preferible no usuar
+let objeto: object = {}
