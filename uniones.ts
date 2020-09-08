d5f668323167f513ac16ace40715358e46207495
Enrique Sotelo Ponce <sotelo.enrique@gmail.com>
2020-09-08 13:44:17 -0500

    Curso TypeScript de Makigas video 22

--- uniones.ts
+++ uniones.ts
@@ -18,3 +18,26 @@ function convertir(valor: string | number) {
     }
 }
 
+// Ejemplo de union discriminante
+type objetoSuma = {
+    operando1: number,
+    operando2: number,
+    tipo: "suma"
+}
+
+type objetoMultiplicacion = {
+    operando1: number,
+    operando2: number,
+    tipo: "multiplicacion"
+}
+
+type operacion = objetoSuma | objetoMultiplicacion
+
+function operar(o: operacion) {
+    // En lugar de usar typeof se ocupa el campo discriminante
+    if (o.tipo == "suma") {
+        return o.operando1 + o.operando2
+    } else {
+        return o.operando1 * o.operando2
+    }
+}
