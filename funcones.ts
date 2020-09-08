da66be6f7045f6c0e710fb1f1db97f8b22b0c04c
Enrique Sotelo Ponce <sotelo.enrique@gmail.com>
2020-09-07 20:11:27 -0500

    Curso TypeScript de Makigas video 11

--- funcones.ts
+++ funcones.ts
@@ -6,4 +6,21 @@ function sumar(m: number, n: number): number {
     return suma
 }
 
-let x: number = sumar(3, 4)
+// Declaración de tipos para los parámetros de una función, forma normal
+let restar: (m: number, n: number) => number = function(m: number, n: number): number {
+    let resta = m - n
+    console.log(`${m} - ${n} = ${resta}`)
+    return resta
+}
+
+// Declaración de tipos para los parámetros de una función
+let dividir: (m: number, n: number) => number
+dividir = (m, n) => m / n
+
+// Declaración de tipos de una función y con una función de callback
+let multiplicar: (a: number, b: number, callback: (r: number) => void) => void
+multiplicar = (a, b, callback) => callback(a * b)
+
+multiplicar(4, 5, function(result) {
+    console.log(`Resultado: ${result}`)
+})
