9d7b12e32273aa9c84ca8b1702305988f1982c3f
Enrique Sotelo Ponce <sotelo.enrique@gmail.com>
2020-09-08 21:10:18 -0500

    Curso TypeScript de Makigas video 25

--- interfaces.ts
+++ interfaces.ts
@@ -1,8 +1,11 @@
 // Declaración y usuo de una interface
 interface UserData {
-    username: string,
-    created_at: Date,
+    readonly username: string,
+    created_at?: Date,
     superuser: boolean,
+
+    logout(): void,
+    rename: (username: string) => void,
 }
 
 function login(): UserData {
@@ -10,7 +13,17 @@ function login(): UserData {
         username: 'admin',
         created_at: new Date(),
         superuser: true,
+
+        logout() {
+            console.log('Adios')
+        },
+        rename(username) {
+            console.log('TODO: Rename account')
+        }
     }
 }
 
 let data = login()
+// data.username = 'foo' no se puede utilizar debido a que la prodipedad es tipo readonly
+data.rename('foo')
+data.created_at?.toISOString() // Evalua si el metodo tiene valor undefined o Date
